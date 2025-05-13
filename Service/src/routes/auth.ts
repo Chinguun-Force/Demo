de53235ee392b0cbb41335e08effadb1850cdf38
24LP4066 24LP4066 <24LP4066@G303-CC0144.local>
2025-05-13 11:57:37 +0800

    nodemailer amjilttai yvuuldg blsn

--- Service/src/routes/auth.ts
+++ Service/src/routes/auth.ts
@@ -1,11 +1,14 @@
 import {Router} from "express"
 import { login, signUp } from "../controller/auth";
-import { checkToken } from "../middleware/checktoken";
+import { resetRequest, updatePassword } from "../controller/reset-password";
+import { verifyToken } from "../middleware/verifytoken";
+// import { resetRequest } from "../controller/reset-password";
 
 const authRouter = Router();
 
 authRouter
 .post('/sign-up', signUp)
 .post('/login', login)
-
+.post('/forgot-password', resetRequest)
+.post("/update-password", verifyToken, updatePassword);
 export { authRouter };
