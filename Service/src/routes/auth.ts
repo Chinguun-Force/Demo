d92584641394095a35247a0fcd76844d9b802a05
Chinguun-Force <bat.chinguun1@gmail.com>
2025-05-14 17:31:35 +0800

    resolved

--- Service/src/routes/auth.ts
+++ Service/src/routes/auth.ts
@@ -1,14 +1,14 @@
-import { Router } from "express"
-import { login, signUp } from "../controller/auth";
-import { resetRequest, updatePassword } from "../controller/reset-password";
+import {Router} from "express"
+import { forgotPassword, login, signUp } from "../controller/auth";
+import { updatePassword } from "../controller/reset-password";
 import { verifyToken } from "../middleware/verifytoken";
 // import { resetRequest } from "../controller/reset-password";
 
 const authRouter = Router();
 
 authRouter
-    .post('/sign-up', signUp)
-    .post('/login', login)
-    .post('/forgot-password', resetRequest)
-    .post("/update-password", verifyToken, updatePassword);
+.post('/sign-up', signUp)
+.post('/login', login)
+.post('/forgot-password', forgotPassword)
+.post("/update-password", verifyToken, updatePassword);
 export { authRouter };
