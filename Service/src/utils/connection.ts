f6462fec561114f4a6a056d59dc189bd0161c2fb
24LP4066 24LP4066 <24LP4066@G303-CC0144.local>
2025-05-16 09:42:32 +0800

    redeploy 122 oroldlogo

--- Service/src/utils/connection.ts
+++ Service/src/utils/connection.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI as string;
 export const connection = async () => {
+    console.log(process.env)
     await mongoose.connect(MONGODB_URI);
     console.log("Connected to database");
 };
